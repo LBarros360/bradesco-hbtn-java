7e6e14d2e9084c0f1a5e029adf182abe23479fd7
LBarros360 <lbarros360@gmail.com>
2025-08-08 17:38:27 +0000

    Task 0, ajuste

--- 0x03/livraria/LivroEdicaoOuro.java
+++ 0x03/livraria/LivroEdicaoOuro.java
@@ -12,7 +12,7 @@ public class LivroEdicaoOuro extends Livro {
 
     @Override
     public double getPreco() {
-        return super.getPreco() * 0.3;
+        return super.getPreco() + (super.getPreco() * 0.3);
     }
 }
 
