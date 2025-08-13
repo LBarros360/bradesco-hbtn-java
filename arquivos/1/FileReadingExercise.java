fcc7a3a9999bc90b5d2554f880ed2598626cd0d3
LBarros360 <lbarros360@gmail.com>
2025-08-13 17:26:43 +0000

    Task 1

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+        //String fileName = "src/exemplo.txt";
+
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String texto;
+            texto = reader.readLine();
+
+            while (texto != null) {
+                System.out.println(texto);
+                texto = reader.readLine();
+            }
+
+            System.out.println("\nLeitura do arquivo concluída.");
+
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
