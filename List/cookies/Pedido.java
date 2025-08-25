03a1617fc7a2b66e8036d2156ae0d09287c3325f
LBarros360 <lbarros360@gmail.com>
2025-08-25 17:22:51 +0000

    Task 1

---
+++ List/cookies/Pedido.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+
+    private List<PedidoCookie> cookies;
+
+    public Pedido() {
+        this.cookies = new ArrayList<>();
+    }
+
+    public void adicionarPedidoCookie(PedidoCookie pedido) {
+
+        this.cookies.add(pedido);
+    }
+
+    public int obterTotalCaixas() {
+        int total = 0;
+
+        for (PedidoCookie c : this.cookies) {
+            total += c.getQuantidadeCaixas();
+        }
+
+        return total;
+    }
+
+    public int removerSabor(String sabor) {
+        int total = 0;
+
+        for (int i = 0; i < this.cookies.size(); i++) {
+            if (this.cookies.get(i).getSabor().equals(sabor)) {
+                total += this.cookies.get(i).getQuantidadeCaixas();
+                this.cookies.remove(i);
+            }
+        }
+        return total;
+    }
+}
+
