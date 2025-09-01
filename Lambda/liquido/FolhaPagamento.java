b1bbc081c6fdd1fdabad179d07de00544674aabf
LBarros360 <lbarros360@gmail.com>
2025-09-01 18:02:51 +0000

    Task 1

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,10 @@
+public class FolhaPagamento {
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculador) {
+
+        return calculador.calcular(funcionario.getSalario());
+
+
+    }
+}
+
