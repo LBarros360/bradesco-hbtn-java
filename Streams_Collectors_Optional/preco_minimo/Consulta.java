8ce401705110a0a980def10e6fce7fa09a624736
LBarros360 <lbarros360@gmail.com>
2025-10-07 21:57:28 +0000

    Task 2

---
+++ Streams_Collectors_Optional/preco_minimo/Consulta.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido (Pedido pedido) {
+
+        Stream<Produto> s = pedido.getProdutos().stream();
+        return s.filter(produto -> produto.getCategoria().equals(CategoriaProduto.LIVRO)).toList();
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> lista) {
+
+        Optional<Produto> p =lista.stream().max(Comparator.comparing(Produto::getPreco));
+        Produto produto = p.get();
+        return produto;
+    }
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> lista, double precoMinimo) {
+
+        return lista.stream().filter(produto -> produto.getPreco() >= precoMinimo).collect(Collectors.toList());
+
+    }
+}
